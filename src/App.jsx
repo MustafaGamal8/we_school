116c9699cc1f5fa2836945cd5c874f740648365b
mustafa <mustafagamal51112@gmail.com>
2023-07-30 17:33:09 +0300

    --loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 
-import './App.css'
-import './index.css'
+import Loader from './components/loader/loader';
+import  "./App.css"
 function App() {
-
-
   return (
     <>
-  <div className='flex flex-col bg-red-400 gap-52 justify-between md:flex-row items-center'>
-
-    <div className='w-[200px] h-[200px] rounded-md border bg-[#000] text-white'>1</div>
-    <div className='w-[200px] h-[200px] rounded-md border bg-[#000] text-white'>2</div>
-    <div className='w-[200px] h-[200px] rounded-md border bg-[#000] text-white'>3</div>
-  </div>
+    <Loader />
+    
     </>
   )
 }
