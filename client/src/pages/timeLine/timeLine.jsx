8fad9c107fc4db10e3ff786616e117e8b955d1e7
karem880 <elalamymee@gmail.com>
2023-08-13 21:51:59 +0200

    -

--- client/src/pages/timeLine/timeLine.jsx
+++ client/src/pages/timeLine/timeLine.jsx
@@ -2,9 +2,30 @@
 
 const TimeLine = () => {
   return ( 
-  <>
-  time line
-  </> );
+    <div className="w-[90%] md:w-[60%] m-auto bg-main flex flex-col justify-between items-center   h-[250px] border-[2px] border-black border-solid rounded-lg mt-[70px] p-2 md:p-5 " >
+    <input
+      type="text"
+      placeholder="أدخل النص هنا"
+      className=" w-full md:w-[95%] p-4 mb-4 rounded-lg text-center outline-none"
+    />
+    <label className=" w-full bg-white text-main px-4 py-2 md:w-[70%] rounded-lg flex items-center justify-center">
+      <FaFileUpload className="mt-2" />
+      <span>تحميل الملف</span>
+      <input
+        type="file"
+        className="hidden"
+        onChange={handleFileChange}
+        accept="*/*"
+      />
+    </label>
+    <button
+      className="w-full md:w-[50%] bg-white text-main px-4 py-2 rounded-lg mt-5"
+      onClick={handleUpload}
+      disabled={!file}
+    >
+      رفع الملف
+    </button>
+  </div>    );
 }
  
 export default TimeLine;
