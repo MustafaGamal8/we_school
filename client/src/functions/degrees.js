f4eed505eaef9764a44b961de1de84ac1fd6516f
mustafa <mustafagamal51112@gmail.com>
2023-08-17 00:01:00 +0300

    --degrees checkPoint

--- client/src/functions/degrees.js
+++ client/src/functions/degrees.js
@@ -1,11 +1,11 @@
 import axios  from 'axios';
 
-const baseUrl = "https://we-school-api.vercel.app/auth"
+const baseUrl = "https://we-school-api.vercel.app"
 
 export const getDegree = async(StudentCode)=>{
   try {
     console.log(StudentCode)
-    const response = await axios.get(`http://localhost:8000/degrees/${StudentCode}`) 
+    const response = await axios.get(baseUrl + `/degrees/${StudentCode}`) 
   return response.data
     
   } catch (error) {
