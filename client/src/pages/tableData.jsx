cfd34a24ae9cddb00dce58bb3a9867d98da6a6dd
karem880 <elalamymee@gmail.com>
2023-08-29 22:37:54 +0200

    -

--- client/src/pages/tableData.jsx
+++ client/src/pages/tableData.jsx
@@ -1,75 +1,78 @@
-import React, { useEffect, useState  } from 'react';
-import axios from 'axios';
-import { toast } from 'react-toastify';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getUsers } from '../functions/users';
 
-
-
-
 const baseUrl = 'https://we-school-api.vercel.app/auth/users';
 
-function TableDataTeatcher() {
-  const [data, setData] = useState([]);
-  
+function TableDataTeacher() {
+    const [data, setData] = useState([]);
 
     const { role } = useParams();
-    
- 
 
     const fetchUsersData = async () => {
-        const response = await getUsers()
-        setData(response)
-        console.log(response)
-    }
-  useEffect(() => {
-    fetchUsersData()     
+        const response = await getUsers();
+        setData(response);
+        console.log(response);
+    };
 
-  }, []);
+    useEffect(() => {
+        fetchUsersData();
+    }, []);
 
-  const handleDelete = (itemId) => {
-    const updatedData = data.filter((item) => item.id !== itemId);
-    setData(updatedData);
-  };
+    const handleDelete = (itemId) => {
+        const updatedData = data.filter((item) => item._id !== itemId);
+        setData(updatedData);
+    };
 
-  return (
-    <div className="flex justify-center">
-      <table className="table-auto w-3/4 mt-8">
-        <thead>
-          <tr className="text-black dark:text-white text-center">
-            <th className="px-4 py-2">ID</th>
-            <th className="px-4 py-2">Name</th>
-            <th className="px-4 py-2">email</th>
-            <th className="px-4 py-2">Role</th>
-            {role === "student" ? (<th className="px-4 py-2">grade</th>):null }
-
-            <th className="px-4 py-2">Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-         role === item.role ?(   <tr key={item.id} className="bg-gray-100 text-center">
-         <td className="border px-4 py-2">{item._id}</td>
-         <td className="border px-4 py-2">{item.firstName} {item.lastName}</td>
-         <td className="border px-4 py-2">{item.email}</td>
-
-         <td className="border px-4 py-2">{item.role}</td>
-         {role === "student" ?(<td className="border px-4 py-2">{item.grade}</td>) :null}
-
-         <td className="border px-4 py-2">
-           <button
-             className="bg-red-500 hover:bg-red-700 text-white px-2 py-1 rounded"
-             onClick={() => handleDelete(item.id)}
-           >
-             Delete
-           </button>
-         </td>
-       </tr>) :null
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+        <div className="flex justify-center">
+            <table className="table-auto w-[90%] mt-8">
+                <thead>
+                    <tr className="text-black dark:text-white text-center bg-gray-300">
+                        <th className="px-4 py-2">ID</th>
+                        <th className="px-4 py-2">Name</th>
+                        <th className="px-4 py-2">Email</th>
+                        <th className="px-4 py-2">Role</th>
+                        {role === 'student' && <th className="px-4 py-2">Grade</th>}
+                        {role === 'teacher' && <th className="px-4 py-2">Upgrade</th>}
+                        <th className="px-4 py-2">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item) =>
+                        role === item.role ? (
+                            <tr key={item._id} className="text-center">
+                                <td className="border px-4 py-2">{item._id}</td>
+                                <td className="border px-4 py-2">
+                                    {item.firstName} {item.lastName}
+                                </td>
+                                <td className="border px-4 py-2">{item.email}</td>
+                                <td className="border px-4 py-2">{item.role}</td>
+                                {role === 'student' && <td className="border px-4 py-2">{item.grade}</td>}
+                                {role === 'teacher' && (
+                                    <td className="border px-4 py-2">
+                                        <button className="bg-blue-500 hover:bg-blue-700 text-white px-2 py-1 rounded">
+                                            Make Admin
+                                        </button>
+                                    </td>
+                                )}
+                                <td className="border px-4 py-2">
+                                    <div className="flex justify-around">
+                                        <button
+                                            className="bg-red-500 hover:bg-red-700 text-white px-2 py-1 rounded"
+                                            onClick={() => handleDelete(item._id)}
+                                        >
+                                            Delete
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ) : null
+                    )}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
-export default TableDataTeatcher;
+export default TableDataTeacher;
