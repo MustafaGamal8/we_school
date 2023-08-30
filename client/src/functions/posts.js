aeb71b42d9958be8e663af76077c6ef3f449f8cc
mustafa <mustafagamal51112@gmail.com>
2023-08-31 00:30:33 +0300

    --fixes

--- client/src/functions/posts.js
+++ client/src/functions/posts.js
@@ -1,41 +1,34 @@
-import axios from 'axios';
+import  axios  from 'axios';
 import { toast } from 'react-toastify';
 
-const baseUrl = 'https://we-school-api.vercel.app';
-const cachedPostsKey = 'cachedPosts';
+const baseUrl = 'https://we-school-api.vercel.app'
 
-export const getPosts = async () => {
+
+export const getPosts = async ()=>{
   try {
-    const cachedPosts = localStorage.getItem(cachedPostsKey);
-
-    if (cachedPosts) {
-      return JSON.parse(cachedPosts);
-    }
-
-    const response = await axios.get(baseUrl + '/posts');
-    const posts = response.data;
-
-    // Cache the fetched posts
-    localStorage.setItem(cachedPostsKey, JSON.stringify(posts));
-
-    return posts;
+    
+  const response = await axios.get(baseUrl + "/posts")
+  return response.data
   } catch (error) {
-    toast.error(error.response?.data?.error || 'An error occurred while fetching posts');
-    return [];
+    return toast.error(error.response.data.error)
+  
+    
   }
-};
+}
 
-export const toggleLike = async (userId, postId) => {
+export const toggleLike = async (userId,postId)=>{
   try {
-    const response = await axios.post(baseUrl + '/posts/toggle-like', {
+    const response = await axios.post(baseUrl+"/posts/toggle-like",{
       userId,
       postId
-    });
-    toast.success(response.data.msg);
+    })
+    return toast.success(response.data.msg)
   } catch (error) {
-    toast.error(error.response?.data?.error || 'An error occurred while toggling like');
+    return toast.error(error.response.data.error)
   }
-};
+}
+
+
 
 export const uploadPost = async (content, files) => {
   const user = JSON.parse(localStorage.getItem('user'));
@@ -58,21 +51,23 @@ export const uploadPost = async (content, files) => {
       toast.success(msg);
     }
 
-    // Clear cached posts after creating a new post
-    localStorage.removeItem(cachedPostsKey);
-
     return true;
   } catch (error) {
-    toast.error(error.response?.data?.error || 'An error occurred while uploading post');
+    toast.error(error.response.data.error);
     return false;
   }
 };
 
-export const deletePost = async (post_id, user_id) => {
+
+
+export const deletePost =  async(post_id,user_id) => {
   try {
-    const response = await axios.delete(baseUrl + `/posts/${post_id}/${user_id}`);
-    toast.success(response.data.msg);
+  const response = await axios.delete(baseUrl + `/posts/${post_id}/${user_id}`)
+  
+   toast.success(response.data.msg)
+
+    
   } catch (error) {
-    toast.error(error.response?.data?.error || 'An error occurred while deleting post');
+     toast.error(error.response.data.error)    
   }
-};
+}
