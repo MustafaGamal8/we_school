f552ba5f8fa5ae19a4c83b54ba4a130199467274
karem880 <elalamymee@gmail.com>
2023-09-09 02:36:11 +0200

    --

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import { Trans } from 'react-i18next';
 
 function Footer() {
   return (
-    <div className="bg-main lg:bg-transparent py-10 md:py-20 lg:px-20 flex flex-col md:flex-row md:justify-between justify-center gap-5 items-center">
+    <footer className="bg-main lg:bg-transparent py-10 md:py-20 lg:px-20 flex flex-col md:flex-row md:justify-between justify-center gap-5 items-center">
       
       <div className="md:w-1/4 mb-10 md:mb-0">
         <div className="flex  items-center mb-6">
@@ -19,26 +19,25 @@ function Footer() {
         </div>
       </div>
 
-      <div className="md:w-1/4 text-white">
+      <div className="md:w-1/4 text-white flex flex-col mr-[98px] md:mr-0 items-start justify-start">
         <h1 className="text-xl font-semibold mb-4"><Trans>اتصل بنا</Trans></h1>
         <ul className="space-y-2">
-          <li><Trans>weschoolmansoura@gmail.com</Trans></li>
+          <li ><Trans>weschoolmansoura@gmail.com</Trans></li>
           <li><Trans>01276071829</Trans></li>
           <li><Trans>01008572255</Trans></li>
           <li><Trans>0100723782</Trans></li>
         </ul>
       </div>
 
-      <div className="md:w-1/4 text-white">
-  <h1 className="text-xl font-semibold mb-4"><Trans>الأقسام</Trans></h1>
-  <ul className="space-y-2">
-    <li><a href="#school_contact"><Trans>اتصل بالمدرسة</Trans></a></li>
-    <li><a href="#school_terms"><Trans>شروط المدرسة</Trans></a></li>
-    <li><a href="#school_debartments"><Trans>أقسام المدرسة</Trans></a></li>
-    <li><a href="#school_partners"><Trans>شركاء المدرسة</Trans></a></li>
-  </ul>
-</div>
-
+      <div className="md:w-1/4  text-white">
+        <h1 className="text-xl font-semibold mb-4"><Trans>الأقسام</Trans></h1>
+        <ul className="space-y-2">
+          <li><Link to="#school_contact"><Trans>اتصل بالمدرسة</Trans></Link></li>
+          <li><Link to="#school_terms"><Trans>شروط المدرسة</Trans></Link></li>
+          <li><Link to="#school_debartments"><Trans>أقسام المدرسة</Trans></Link></li>
+          <li><Link to="#school_partners"><Trans>شركاء المدرسة</Trans></Link></li>
+        </ul>
+      </div>
 
       <div className="md:w-1/4 text-white">
         <h1 className="text-xl font-semibold mb-4"><Trans>المطورون</Trans></h1>
@@ -48,7 +47,7 @@ function Footer() {
           <li><Link to="/about" className="text-lg text-blue-500 hover:underline"><Trans>عنهم</Trans></Link></li>
         </ul>
       </div>
-    </div>
+    </footer>
   );
 }
 
