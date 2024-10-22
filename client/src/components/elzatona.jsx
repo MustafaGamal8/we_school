8cfeca956256c349fe6356fc09dc7ef2f4033d8e
Mustafa <mustafa.gamal.elsayed@gmail.com>
2024-10-23 00:03:20 +0300

    fix

--- client/src/components/elzatona.jsx
+++ client/src/components/elzatona.jsx
@@ -20,14 +20,14 @@ export default function ElzatonaModal() {
       {isOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
           <div className="bg-white p-8 rounded-lg max-w-lg w-full">
-            <h1>POV: اللي مستنيك بعد المدرسة </h1>
-            <video src="/assets/wow.mp4" autoPlay controls className="w-full" />
-            <button 
+            <h1 className="text-2xl font-bold mb-4 text-center">POV: اللي مستنيك بعد المدرسة </h1>
+            <video src="/assets/wow.mp4" autoPlay  className="w-full" />
+            <div 
               onClick={toggleModal} 
-              className="bg-red-500 text-white px-4 py-2 rounded-lg"
+              className="bg-red-500 text-white px-4 py-2 rounded-lg mx-auto text-center mt-2  cursor-pointer"
             >
               أقفل
-            </button>
+            </div>
           </div>
         </div>
       )}
